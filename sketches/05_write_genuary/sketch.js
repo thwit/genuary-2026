06cdbd4aee588e18b00dc327a9d2a9d7a8fbc354
thw <thomaswitting97@gmail.com>
2026-01-04 19:01:18 +0100

    genuary 5

--- sketches/05_write_genuary/sketch.js
+++ sketches/05_write_genuary/sketch.js
@@ -1,65 +1,55 @@
-let img;
+let N = 60;    // number of columns
+let M = 60;     // number of rows
+let K = 12;    // font size
 
 async function setup() {
-  img = await loadImage('Untitled.png');
-  img.resize(int(img.width / 3.2), int(img.height / 3.2))
-  createCanvas(img.width, img.height);
+  createCanvas(600, 600);
+  
+  // Load font asynchronously
+  customFont = await loadFont("1942.ttf");
+
+  textFont(customFont);
+
+  textAlign(CENTER, CENTER);
   noLoop();
 }
 
 function draw() {
-  background(220);
+  background(255);
 
-  img.loadPixels();
+  let cellW = width / N;
+  let cellH = height / M;
 
-  const w = img.width;
-  const h = img.height;
+  let k = 0;
+  let letters = ["g", "e", "n", "u", "a", "r", "y"];
+  let help = ["h", "e", "l", "p"]
 
-  // Iterate column by column
-  for (let x = 0; x < w; x++) {
-    let columnPixels = [];
+  for (let i = 3; i < N-3; i++) {
+    for (let j = 3; j < M-3; j++) {
 
-    // Extract all pixels in this column
-    for (let y = 0; y < h; y++) {
-      let index = 4 * (y * w + x);
+      // Random uppercase letter A–Z
+      let letter = String.fromCharCode(97 + floor(random(26)));
 
-      // Store pixel data
-      columnPixels.push({
-        r: img.pixels[index + 0],
-        g: img.pixels[index + 1],
-        b: img.pixels[index + 2],
-        a: img.pixels[index + 3],
-        brightness: brightnessFromRGB(
-          img.pixels[index + 0],
-          img.pixels[index + 1],
-          img.pixels[index + 2]
-        )
-      });
-    }
+      if (noise(i*0.05, j*0.05) < (random(-0.1, 0.1) + .375)) {
+        letter = letters[k % letters.length];
+      }
 
-    // Sort this column by brightness
-    columnPixels.sort((a, b) => a.brightness - b.brightness);
+      if (noise(i*0.05, j*0.05) > (random(-0.1, 0.1) + .55)) {
+        letter = help[k % help.length];
+      }
+      
+      if (noise(100+i*0.05, j*0.03) + random(-0.1, 0.1) < 0.25) {
+        continue;
+      }
 
-    // Write sorted pixels back into the image
-    for (let y = 0; y < h; y++) {
-      let index = 4 * (y * w + x);
-      let p = columnPixels[y];
+      console.log(letter)
 
+      let y = i * cellW + cellW / 2;
+      let x = j * cellH + cellH / 2;
 
-      img.pixels[index + 0] = p.r;
-      img.pixels[index + 1] = p.g;
-      img.pixels[index + 2] = p.b;
-      img.pixels[index + 3] = p.a;
+      textSize(K + noise(i*0.05, 400 + j*0.05)*24 + random(-4, 4));
+      text(letter, x + random(-2, 2), y + random(-2, 2));
+      k++;
     }
   }
-
-  img.updatePixels();
-  image(img, 0, 0);
-}
-
-
-// Simple perceptual brightness function
-function brightnessFromRGB(r, g, b) {
-  return 255-max(b + random(-125, 125), max(r + random(-125, 125),g + random(-125, 125)))
-  return 0.299 * r + 0.e587 * g + 0.114 * b;
 }
